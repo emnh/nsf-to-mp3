1a60486c5524ae032a310cb1238e5168889533f7
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2024-03-02 23:50:09 +0100

    parallel

--- nsf_to_mp3.sh
+++ nsf_to_mp3.sh
@@ -3,14 +3,19 @@ file=$1
 songs=$(nosefart -i $file | fgrep "Number of Songs" | cut -f2 -d:)
 echo $songs
 out=output
-mkdir -p $out
+mkdir -p $out cmds
 bname=$(basename $file | sed 's/\.nsf$//')
 touch $out/$bname.txt
 for i in `seq 1 $songs`; do
   wav="$out/${bname}_$i.wav"
-  wine ~/Downloads/nsfplay/nsfplay.exe "$file" "$wav" "$i"
+  echo wine ~/Downloads/nsfplay/nsfplay.exe \"$file\" \"$wav\" \"$i\" >> cmds/nsfplay_commands.txt
+  echo ffmpeg -y -i \"$wav\" -c:a libopus -b:a 128k \"$out/${bname}_$i.opus\" >> cmds/ffmpeg_commands.txt
   #ffmpeg -i "$wav" "$out/${bname}_$i.mp3"
-  ffmpeg -y -i "$wav" -c:a libopus -b:a 128k "$out/${bname}_$i.opus"
-  echo file "'"${bname}_$i.mp3"'" >> $out/$bname.txt
+  #echo file "'"${bname}_$i.mp3"'" >> $out/$bname.txt
 done
 #ffmpeg -f concat -i $out/$bname.txt -c copy "done"/$bname.mp3
+
+# Run this first
+# time ( for file in *.nsf; do ~/data/devel/nsf-to-mp3/nsf_to_mp3.sh $file; done )
+# Run this after
+# N=12 time ( ( cat cmds/nsfplay_commands.txt | parallel -j $N eval ) && ( cat cmds/ffmpeg_commands.txt| parallel -j $N eval ) )
