173808c16ac7c3fff7562b19b90e6eed0f7bb6d4
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2024-03-02 22:52:19 +0100

    upd

--- nsf_to_mp3.sh
+++ nsf_to_mp3.sh
@@ -4,12 +4,13 @@ songs=$(nosefart -i $file | fgrep "Number of Songs" | cut -f2 -d:)
 echo $songs
 out=output
 mkdir -p $out
-bname=$(basename $file | sed s/\.nsf//)
+bname=$(basename $file | sed 's/\.nsf$//')
 touch $out/$bname.txt
 for i in `seq 1 $songs`; do
-  wav=$out/${bname}_$i.wav
-  wine ~/Downloads/nsfplay/nsfplay.exe $file $wav $i
-  ffmpeg -i $wav $out/${bname}_$i.mp3
+  wav="$out/${bname}_$i.wav"
+  wine ~/Downloads/nsfplay/nsfplay.exe "$file" "$wav" "$i"
+  #ffmpeg -i "$wav" "$out/${bname}_$i.mp3"
+  ffmpeg -i "$wav" -c:a libopus -b:a 128k "$out/${bname}_$i.opus"
   echo file "'"${bname}_$i.mp3"'" >> $out/$bname.txt
 done
-ffmpeg -f concat -i $out/$bname.txt -c copy "done"/$bname.mp3
+#ffmpeg -f concat -i $out/$bname.txt -c copy "done"/$bname.mp3
